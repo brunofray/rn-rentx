0a79e175fd3fcb56ab2df2f4b878bb2de17c20fd
Bruno Fray <brunofray@gmail.com>
2022-06-07 17:59:19 -0300

    Tema Global da Aplicação

--- App.tsx
+++ App.tsx
@@ -1,6 +1,38 @@
 import React from 'react';
+import { View, Text } from 'react-native';
+import { ThemeProvider } from 'styled-components';
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from '@expo-google-fonts/inter';
+
+import {
+  Archivo_400Regular,
+  Archivo_500Medium,
+  Archivo_600SemiBold,
+} from '@expo-google-fonts/archivo';
+
 import { Home } from './src/screens/Home';
+import theme from './src/styles/theme';
 
 export default function App() {
-  return <Home />
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Archivo_400Regular,
+    Archivo_500Medium,
+    Archivo_600SemiBold,
+  });
+
+  if (!fontsLoaded) {
+    return <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}><Text>Carregando...</Text></View>
+  }
+
+  return (
+    <ThemeProvider theme={theme}>
+        <Home />
+    </ThemeProvider>
+  )
 }
