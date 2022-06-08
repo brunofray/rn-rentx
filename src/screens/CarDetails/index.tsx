262d7e00580a987bbb8e648cab6a6bae8a20fdac
Bruno Fray <brunofray@gmail.com>
2022-06-07 23:14:34 -0300

    Continuando a interface de Detalhes

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -7,6 +7,15 @@ import {
   Container,
   Header,
   CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
 } from './styles';
 
 export function CarDetails(){
@@ -21,6 +30,26 @@ export function CarDetails(){
           imagesUrl={['https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png']}
         />
       </CarImages>
+
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Lamborghini</Brand>
+            <Name>Huracan</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <About>
+          Este é automóvel desportivo. 
+          Surgiu do lendário touro de lide indultado na praça Real Maestranza de Sevilla.
+          É um belíssimo carro para quem gosta de acelerar.
+        </About>
+      </Content>
     </Container>
   );
 }
