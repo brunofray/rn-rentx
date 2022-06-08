0469e28b5c5a10fdf1d3ebbe271a6d74b8a5d276
Bruno Fray <brunofray@gmail.com>
2022-06-07 22:43:02 -0300

    Componente de Slider

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
+
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
 import {
   Container,
   Header,
+  CarImages,
 } from './styles';
 
 export function CarDetails(){
@@ -12,6 +15,12 @@ export function CarDetails(){
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+
+      <CarImages>
+        <ImageSlider
+          imagesUrl={['https://www.pngmart.com/files/1/Audi-RS5-Red-PNG.png']}
+        />
+      </CarImages>
     </Container>
   );
 }
