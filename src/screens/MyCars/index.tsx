2cd5bf21cf7074ae7d30ee9bdffee99f07d6550b
Bruno Fray <brunofray@gmail.com>
2022-06-09 12:26:47 -0300

    Exibindo Carros Agendados

--- src/screens/MyCars/index.tsx
+++ src/screens/MyCars/index.tsx
@@ -1,14 +1,43 @@
 import React, { useEffect, useState } from 'react';
+import { StatusBar, View } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { useTheme } from 'styled-components';
+
 import { CarDTO } from '../../dtos/CarDTO';
+import { BackButton } from '../../components/BackButton';
+import { Car } from '../../components/Car';
+import { Load } from '../../components/Load';
+
 import api from '../../services/api';
 
 import {
   Container,
+  Header,
+  Title,
+  SubTitle,
+  Content,
+  Appointments,
+  AppointmentsTitle,
+  AppointmentsQuantity,
+  CarList,
 } from './styles';
 
+interface CarProps {
+  id: string;
+  user_id: string;
+  car: CarDTO;
+}
+
 export function MyCars(){
-  const [cars, setCars] = useState<CarDTO[]>([]);
+  const [cars, setCars] = useState<CarProps[]>([]);
   const [loading, setLoading] = useState(true);
+
+  const theme = useTheme();
+  const navigation = useNavigation();
+
+  function handleBack() {
+    navigation.goBack();
+  }
 
   useEffect(() => {
     async function fetchCars() {
@@ -27,7 +56,49 @@ export function MyCars(){
 
   return (
     <Container>
+      <Header>
+        <StatusBar
+          barStyle="light-content"
+          translucent
+          backgroundColor="transparent"
+        />
 
+        <View style={{ alignSelf: 'flex-start' }}>
+          <BackButton 
+            onPress={handleBack}
+            color={theme.colors.shape}
+          />
+        </View>
+
+        <Title>
+          Escolha uma {'\n'}
+          data de início e {'\n'}
+          fim do aluguel
+        </Title>
+
+        <SubTitle>
+          Conforto, segurança e praticidade.
+        </SubTitle>
+      </Header>
+
+      <Content>
+        <Appointments>
+          <AppointmentsTitle>Agendamentos feitos</AppointmentsTitle>
+          <AppointmentsQuantity>{String(cars.length).padStart(2, '0')}</AppointmentsQuantity>
+        </Appointments>
+
+      { 
+        loading ? <Load /> :
+        <CarList
+          data={cars}
+          keyExtractor={item => String(item.id)}
+          renderItem={({ item }) => 
+            <Car data={item.car} />
+          }
+        />
+      }
+
+      </Content>
     </Container>
   );
 }
