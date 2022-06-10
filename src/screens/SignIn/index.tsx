254811bd37ba81a1cd3f0cf52aa2e6c7dc0068e3
Bruno Fray <brunofray@gmail.com>
2022-06-10 12:33:19 -0300

    Input de Senha

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -4,6 +4,7 @@ import { useTheme } from 'styled-components';
 
 import { Button } from '../../components/Button';
 import { Input } from '../../components/Input';
+import { PasswordInput } from '../../components/PasswordInput';
 
 import {
   Container,
@@ -45,7 +46,10 @@ export function SignIn(){
           autoCorrect={false}
           autoCapitalize="none"
         />
-        <Input 
+
+        <SpaceBottom marginBottom={8} />
+
+        <PasswordInput 
           iconName="lock"
           placeholder="Senha"
         />
@@ -58,7 +62,9 @@ export function SignIn(){
           enabled={true}
           loading={false}
         />
+
         <SpaceBottom marginBottom={8} />
+
         <Button
           title="Criar conta gratuita"
           color={theme.colors.background_secondary}
