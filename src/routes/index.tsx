f92be4929acbb05b5e4c09570feab042515f116d
Bruno Fray <brunofray@gmail.com>
2022-06-11 02:26:17 -0300

    Rotas Privadas e Públicas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,42 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
+import { useAuth } from '../hooks/auth';
 
-import { StackRoutes } from './stack.routes';
+import { AppTabRoutes } from './app.tab.routes';
+import { AuthRoutes } from './auth.routes';
+
+import { ConfirmationParams } from '../screens/Confirmation';
+import { CarDTO } from "../dtos/CarDTO";
+import { UserDTO } from "../dtos/UserDTO";
+
+export type AppRoutes = {
+  Splash: undefined;
+  SignIn: undefined;
+  SignUpFirstStep: undefined;
+  SignUpSecondStep: { user: UserDTO};
+  Home: undefined;
+  CarDetails: { car: CarDTO };
+  Scheduling: { car: CarDTO };
+  SchedulingDetails: { 
+    car: CarDTO;
+    dates: string[];
+  };
+  Confirmation: ConfirmationParams;
+  MyCars: undefined;
+}
+
+declare global {
+  namespace ReactNavigation {
+    interface RootParamList extends AppRoutes{}
+  }
+}
 
 export function Routes(){
+  const { user } = useAuth();
+
   return (
     <NavigationContainer>
-      <StackRoutes />
+      { user ? <AppTabRoutes /> : <AuthRoutes /> }
     </NavigationContainer>
   );
 }
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,20 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { useFocusEffect, useNavigation } from '@react-navigation/native';
-import { useTheme } from 'styled-components';
-import { StatusBar, StyleSheet, BackHandler } from 'react-native';
-import { Ionicons } from '@expo/vector-icons';
+import { useNavigation } from '@react-navigation/native';
+import { StatusBar, StyleSheet } from 'react-native';
 import { RFValue } from 'react-native-responsive-fontsize';
-
-import { RectButton, PanGestureHandler } from 'react-native-gesture-handler';
-
-import Animated, {
-  useSharedValue,
-  useAnimatedStyle,
-  useAnimatedGestureHandler,
-  withSpring,
-} from 'react-native-reanimated';
-
-const ButtonAnimated = Animated.createAnimatedComponent(RectButton);
 
 import Logo from '../../assets/logo.svg';
 import api from '../../services/api';
@@ -34,43 +21,11 @@ import {
 export function Home(){
   const [cars, setCars] = useState<CarDTO[]>([]);
   const [loading, setLoading] = useState(true);
-  const theme = useTheme();
-
-  const positionY = useSharedValue(0);
-  const positionX = useSharedValue(0);
-
-  const myCarsButtonStyle = useAnimatedStyle(() => {
-    return {
-      transform: [
-        { translateX: positionX.value },
-        { translateY: positionY.value }
-      ],
-    }
-  });
-
-  const onGestureEvent = useAnimatedGestureHandler({
-    onStart(_, ctx: any){
-      ctx.positionX = positionX.value;
-      ctx.positionY = positionY.value;
-    },
-    onActive(event, ctx){
-      positionX.value = ctx.positionX + event.translationX;
-      positionY.value = ctx.positionY + event.translationY;
-    },
-    onEnd(){
-      positionX.value = withSpring(0);
-      positionY.value = withSpring(0);
-    }
-  });
   
   const navigation = useNavigation();
 
   function handleCarDetails(car: CarDTO) {
     navigation.navigate('CarDetails', { car });
-  }
-
-  function handleOpenMyCars() {
-    navigation.navigate('MyCars');
   }
 
   useEffect(() => {
@@ -87,14 +42,6 @@ export function Home(){
 
     fetchCars();
   }, []);
-
-  useFocusEffect(() => {
-      const backHandler = BackHandler.addEventListener(
-        'hardwareBackPress',
-        () => true
-      );
-      return () => backHandler.remove();
-  });
 
   return (
     <Container>
@@ -128,42 +75,6 @@ export function Home(){
           }
         />
       }
-
-      <PanGestureHandler onGestureEvent={onGestureEvent}>
-        <Animated.View
-          style={[
-            myCarsButtonStyle,
-            {
-              position: 'absolute',
-              bottom: 13,
-              right: 22,
-            }
-          ]}
-        >
-          <ButtonAnimated 
-            onPress={handleOpenMyCars}
-            style={[styles.button, { backgroundColor: theme.colors.main }]}
-          >
-            <Ionicons 
-              name="ios-car-sport"
-              size={32}
-              color={theme.colors.shape}
-            />
-          </ButtonAnimated>
-        </Animated.View>
-      </PanGestureHandler>
     </Container>
   );
 }
-
-export const styles = StyleSheet.create({
-  button: {
-    width: 60,
-    height: 60,
-
-    borderRadius: 30,
-  
-    justifyContent: 'center',
-    alignItems: 'center',
-  }
-});
