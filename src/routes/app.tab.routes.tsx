551ce8020d81e39d2cd0def39f80befe55dd9ff6
Bruno Fray <brunofray@gmail.com>
2022-06-12 03:32:17 -0300

    Header do Profile

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -7,9 +7,9 @@ import CarSvg from '../assets/car.svg';
 import PeopleSvg from '../assets/people.svg';
 
 import { AppStackRoutes } from './app.stack.routes';
-import { Home } from '../screens/Home';
 import { MyCars } from "../screens/MyCars";
 import { Platform } from "react-native";
+import { Profile } from "../screens/Profile";
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
@@ -51,7 +51,7 @@ export function AppTabRoutes() {
       />
       <Screen
         name="Profile"
-        component={Home}
+        component={Profile}
         options={{
           tabBarIcon: (({ color }) => (
             <PeopleSvg width={24} height={24} fill={color} />
