be993e5eab7c9d30d007565d1168d9904e495879
Bruno Fray <brunofray@gmail.com>
2022-06-13 01:08:24 -0300

    Fix loading - auth

--- src/hooks/auth.tsx
+++ src/hooks/auth.tsx
@@ -114,8 +114,8 @@ function AuthProvider({ children }: AuthProviderProps) {
         const userData = response[0]._raw as unknown as User;
         api.defaults.headers.common['Authorization'] = `Bearer ${userData.token}`;
         setData( userData );
-        setLoading(false);
       }
+      setLoading(false);
     }
 
     loadUserData();
